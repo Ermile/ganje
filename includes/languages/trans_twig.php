a2d2a0063f8a8994ea5c6e81d77ebc9721917bc6
Javad <J.Evazzadeh@live.com>
2016-08-14 16:53:12 +0430

    init

---
+++ includes/languages/trans_twig.php
@@ -0,0 +1,63 @@
+<?php
+private function transtext()
+{
+
+	//-----------------------------------------------project\content\home\display.html
+	echo T_("Home");                                                                  // Line 17
+	echo T_("Slogan");                                                                // Line 18
+	echo T_("About");                                                                 // Line 188
+	echo T_("Contact");                                                               // Line 20
+	echo T_("Documentation");                                                         // Line 44
+	echo T_("Check on Github");                                                       // Line 157
+	echo T_("Download Latest Version");                                               // Line 156
+	echo T_("Language List");                                                         // Line 29
+	echo T_("Find Out More");                                                         // Line 42
+	echo T_("Secure, Fast and Flexible");                                             // Line 67
+	echo T_("Our story in timeline");                                                 // Line 77
+	echo T_("2006-2010");                                                             // Line 89
+	echo T_("Our Friendship Beginnings");                                             // Line 90
+	echo T_("We find each other in Iran national skills competition that knowns as"); // Line 93
+	echo T_("Worldskills");                                                           // Line 93
+	echo T_("Javad Evazzadeh Kakroudi");                                              // Line 93
+	echo T_("Saman Soltani");                                                         // Line 93
+	echo T_("Hasan Salehi Hajiabadi");                                                // Line 93
+	echo T_("and");                                                                   // Line 93
+	echo T_("Omid Pilevar");                                                          // Line 93
+	echo T_("2013");                                                                  // Line 8 Seperate
+	echo T_("Framework is Born");                                                     // Line 104
+	echo T_("Idea of create PHP framework was started");                              // Line 107
+	echo T_("Hasan selected to create framework basics.");                            // Line 107
+	echo T_("November 2014");                                                         // Line 117
+	echo T_("First Service");                                                         // Line 118
+	echo T_("First project is launching with this framework");                        // Line 121
+	echo T_("September 2015");                                                        // Line 131
+	echo T_("Ermile");                                                                // Line 189
+	echo T_("We can doing hard job!");                                                // Line 135
+	echo T_("We can register Ermile trademark:)");                                    // Line 135
+	echo T_("To be <br />continue...");                                               // Line 141
+	echo T_("Location");                                                              // Line 176
+	echo T_("#83 Moallem St");                                                        // Line 177
+	echo T_("Qom, IRAN");                                                             // Line 177
+	echo T_("+98 25 3773 5183");                                                      // Line 177
+	echo T_("Another Languages");                                                     // Line 180
+	echo T_(" is a free to use, open source PHP framework created by");               // Line 189
+	echo T_("Copyright");                                                             // Line 201
+
+	//--------------------------------------------project\content\home\layout-xhr.html
+	echo T_("View your current location and navigate to parent of it");               // Line 9
+
+	//-------------------------------------------project\content\template\dafault.html
+	echo T_("Some right reserved!");                                                  // Line 28
+
+	//------------------------------------project\includes\languages\trans_static.html
+	echo T_("Saloos");                                                                // Line 7 Seperate
+	echo T_("2014");                                                                  // Line 9 Seperate
+	echo T_("2015");                                                                  // Line 10 Seperate
+	echo T_("2016");                                                                  // Line 11 Seperate
+	echo T_("2017");                                                                  // Line 12 Seperate
+	echo T_("2018");                                                                  // Line 13 Seperate
+	echo T_("2019");                                                                  // Line 14 Seperate
+	echo T_(" ");                                                                     // Line 15 Seperate
+
+}
+?>
---
+++ includes/mvc/view.php
@@ -0,0 +1,37 @@
+<?php
+namespace mvc;
+
+class view extends \lib\mvc\view
+{
+	function _construct()
+	{
+		// define default value for global
+
+		$this->data->site['title']   = T_("Ganje");
+		$this->data->site['desc']    = T_("Free & open source attendance service!");
+		$this->data->site['slogan']  = T_("Enjoy work time");
+
+		$this->data->page['desc']    = $this->data->site['slogan'];
+
+		// if(! ($this->url('sub') === 'cp' || $this->url('sub') === 'account') )
+		// 	$this->url->MainStatic       = false;
+
+		/*
+		// add language list for use in display
+		$this->global->langlist		= array(
+												'fa_IR' => 'فارسی',
+												'en_US' => 'English',
+												'de_DE' => 'Deutsch'
+												);
+
+
+		// if you need to set a class for body element in html add in this value
+		$this->data->bodyclass      = null;
+		*/
+
+		if(method_exists($this, 'options')){
+			$this->options();
+		}
+	}
+}
+?>
