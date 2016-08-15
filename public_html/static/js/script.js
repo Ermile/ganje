6600c031c4a3777bcf6107b22bf4d26878edcf36
Ahmad <ahmadkarimi1991@gmail.com>
2016-08-15 19:47:48 +0430

    add new slider

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -1,3 +1,31 @@
-$(document).ready(function() {
-  console.log(1);
-});
+function startTime() {
+  var today = new Date();
+  var h     = today.getHours();
+  var m     = today.getMinutes();
+  var s     = today.getSeconds();
+
+  m = checkTime(m);
+  s = checkTime(s);
+
+  $('.hour').html(h);
+  $('.minute').html(m);
+  $('.second').html(s);
+
+  var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  };
+  return i;
+}
+
+// $(document).ready(function() {
+//   startTime();
+
+//   $('button').click(function() {
+//     $('.pt-page-1').addClass('pt-page-scaleDown');
+//     $('.pt-page-2').addClass('pt-page-scaleUpDown pt-page-delay300');
+//   });
+// });
