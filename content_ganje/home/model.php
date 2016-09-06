a438aed8698e874f29b38c829a9604fc93bd37ef
Javad <J.Evazzadeh@live.com>
2016-09-06 18:46:24 +0430

    fix show time and telegram messages

--- content_ganje/home/model.php
+++ content_ganje/home/model.php
@@ -3,29 +3,31 @@ namespace content_ganje\home;
 use \lib\utility;
 use \lib\debug;
 use \lib\db;
+use \lib\utility\jdate;
 use \lib\telegram\tg as bot;
 
 class model extends \mvc\model
 {
 
 	public $user_id;
+	public $start;
 	public $plus;
 	public $minus;
-	public $lang = 'fa';
 
-	public function post_hours(){
 
+	public function post_hours()
+	{
 		$type   = utility::post("type");
 		$result = null;
 
-		switch ($type) {
+		switch ($type)
+		{
 			case 'summary':
 				$this->summary();
 				return ;
 				break;
 
 			default:
-				# code...
 				break;
 		}
 		//----------- get value
@@ -34,7 +36,8 @@ class model extends \mvc\model
 		$this->minus   = (utility::post('minus') == null) ? 0 : utility::post('minus');
 
 		//----------- check users status
-		if($this->check_user()){
+		if($this->check_user())
+		{
 			//------- set time
 			$result = $this->set_time();
 		}
@@ -43,12 +46,13 @@ class model extends \mvc\model
 		debug::property('result', $result);
 	}
 
+
 	/*
 	* check users status
 	*/
-	public function check_user(){
-		$query = "
-				SELECT
+	public function check_user()
+	{
+		$query = "SELECT
 					user_status
 				FROM users
 				WHERE id = {$this->user_id}
@@ -56,7 +60,8 @@ class model extends \mvc\model
 
 		$check_user = db::get($query, "user_status", true);
 
-		if($check_user != "active"){
+		if($check_user != "active")
+		{
 			debug::error(T_("User not found"));
 			return false;
 		}
@@ -64,71 +69,44 @@ class model extends \mvc\model
 		return 'true';
 	}
 
-	public function set_time(){
 
-		$date = date("Y-m-d");
-
-
-		$time = date("H:i:s");
-
+	/**
+	 * set time of enter or exit
+	 */
+	public function set_time()
+	{
+		$today = date("Y-m-d");
+		$time  = date("H:i");
 		$query = "SELECT * FROM hours
 					WHERE
 						user_id   = {$this->user_id} AND
-						hour_date = '$date' AND
+						hour_date = '$today' AND
 						hour_end is null
 					LIMIT 1";
 
 		$check_date = db::get($query, null, true);
 
-
-		$displayname = \lib\db\users::get_one($this->user_id);
-		$displayname = preg_replace("/\s/", "_", T_($displayname['displayname']));
-
-		$telegram_date = date("Y_m_d");
-		if($this->lang == 'fa'){
-			$telegram_date =  \lib\utility\jdate::date("Y_m_d");
-		}
-
-
-		if($check_date == null) {
-
-			$enter_msg = "#" . T_("Enter") . "\n";
-			$enter_msg .= "#" . $displayname. "\n";
-			$enter_msg .= T_("Registered"). "\n";
-			$enter_msg .= "#" .  $telegram_date . "\n";
-
-
+		if($check_date == null)
+		{
 			//----- add firs time in day
 			$insert = "INSERT INTO hours
 						SET user_id = {$this->user_id},
-							hour_date = '$date',
+							hour_date = '$today',
 							hour_start = '$time'
 							";
 
 			db::query($insert);
 
-			$tg = self::send_telegram($enter_msg);
+			// send message from telegram
+			self::generate_telegram_text('enter');
 			debug::true(T_("Enter was registered."). ' '. T_("Have a good time."));
 			return 'enter';
 
-		}elseif($check_date['hour_end'] == null){
-
-
-			$exit_msg = "#". T_("Exit"). "\n";
-			$exit_msg .= "#". $displayname. "\n";
-			$exit_msg .= T_("in"). ' '. $time. "\n";
-			$exit_msg .= "#".  $telegram_date. "\n";
-			$exit_msg .= "📊 \n";
-			$exit_msg .= T_("from"). " ". $check_date['hour_start']. ' '. T_("to"). ' '. $time. "\n" ;
-
-			if($this->minus > 0){
-				$exit_msg .= "😡 ". T_("minus"). ": ". $this->minus. "\n";
-			}
-
-			if($this->plus > 0 ){
-				$exit_msg .= "😘 ". T_("plus"). ": ". $this->plus. "\n";
-			}
-
+		}
+		elseif($check_date['hour_end'] == null)
+		{
+			// set start time
+			$this->start = strtotime("$today ". $check_date['hour_start']);
 			//------- add end time
 			$update = "UPDATE hours
 						SET hour_end = '$time',
@@ -143,31 +121,31 @@ class model extends \mvc\model
 							id = {$check_date['id']} ";
 
 			db::query($update);
-
-			$tg = self::send_telegram($exit_msg);
-			self::list_online();
-			debug::true(T_("Bye Bye;)"));
+			self::generate_telegram_text('exit');
+			debug::warn(T_("Bye Bye;)"));
 			return 'exit';
-
 		}
-
 	}
 
 
 	/*
 	* get list of users to show
 	*/
-	public function post_list(){
-
+	public function post_list()
+	{
 		return array('list' => \lib\db\users::get_all(), 'summary' => $this->summary());
-
 	}
 
-	public function summary() {
 
-		$date = date("Y-m-d");
+	/**
+	 * [summary description]
+	 * @return [type] [description]
+	 */
+	public function summary()
+	{
 
-		$report = array();
+		$today  = date("Y-m-d");
+		$report = [];
 
 		//--------- repeat to every query
 		$field = "users.id,users.user_displayname as displayname,
@@ -181,26 +159,24 @@ class model extends \mvc\model
 				  INNER JOIN users on hours.user_id = users.id
 				  WHERE 1 ";
 
-
-		$qry =
-		"SELECT $field,
+		$qry = "SELECT $field,
 			'daily' as type
 			$join
-			AND hours.hour_date = '$date'
+			AND hours.hour_date = '$today'
 			GROUP BY
 				hours.user_id,
 				hours.hour_date
 		";
 
-		if($this->lang == 'fa'){
-
-			$jalali_month = \lib\utility\jdate::date("m",false, false);
-			$jalali_year = \lib\utility\jdate::date("Y",false, false);
+		if(substr(\lib\router::get_storage('language'), 0, 2) === 'fa')
+		{
+			$jalali_month = jdate::date("m",false, false);
+			$jalali_year  = jdate::date("Y",false, false);
 
 			list($start_date, $end_date) = \lib\db\date::convert_month($jalali_year, $jalali_month);
 
 			$start_week = date("Y-m-d", strtotime("last Saturday", time()));
-			$end_week = date("Y-m-d", strtotime("Saturday", time()));
+			$end_week   = date("Y-m-d", strtotime("Saturday", time()));
 
 			$qry .= "
 			UNION
@@ -216,7 +192,9 @@ class model extends \mvc\model
 			AND (hours.hour_date >= '$start_date' AND hours.hour_date < '$end_date')
 			GROUP BY hours.user_id";
 
-		}else{
+		}
+		else
+		{
 			$qry .= "
 			UNION
 				SELECT $field,
@@ -233,22 +211,21 @@ class model extends \mvc\model
 		}
 
 		$report = db::get($qry);
-
-		$return  = array();
-		foreach ($report as $key => $value) {
+		$return = array();
+		foreach ($report as $key => $value)
+		{
 			$id = $value['id'];
-			if(!isset($return[$id])) {
-
-				$return[$id] = [];
-				$return[$id]['id'] = $id;
+			if(!isset($return[$id]))
+			{
+				$return[$id]         = [];
+				$return[$id]['id']   = $id;
 				$return[$id]['name'] = $value['displayname'];
 			}
 
-			$return[$id][$value['type']]['diff'] = $value['diff'];
-			$return[$id][$value['type']]['plus'] = $value['plus'];
+			$return[$id][$value['type']]['diff']  = $value['diff'];
+			$return[$id][$value['type']]['plus']  = $value['plus'];
 			$return[$id][$value['type']]['minus'] = $value['minus'];
 			$return[$id][$value['type']]['total'] = $value['total'];
-
 		}
 		return $return;
 	}
@@ -272,19 +249,85 @@ class model extends \mvc\model
 
 		];
 		$result = bot::sendResponse($msg);
+
 		return $result;
 	}
 
-	public static function list_online(){
+
+	/**
+	 * generate telegram messages and call send function to send messages directly
+	 * @param  [type] $_type [description]
+	 * @return [type]        [description]
+	 */
+	public function generate_telegram_text($_type)
+	{
+		$msg      = '';
+		$date_now = \lib\utility::date("l j F Y", false, 'default');
+		$time_now = \lib\utility::date("H:i", false, 'default');
+		$name     = \lib\db\users::get_one($this->user_id);
+		$name     = "*". T_($name['displayname']). "*";
+
+		switch ($_type)
+		{
+			case 'enter':
+				// if this person is first one in this day send current date
+				if(\lib\db\users::check_first())
+				{
+					$tg = self::send_telegram($date_now);
+				}
+				$msg = "✅ $name $time_now";
+				break;
+
+
+			case 'exit':
+				$msg        = "💤 $name\n";
+				$total      = floor(abs(strtotime('now') - $this->start) / 60);
+				var_dump($total);
+				if($total < 1)
+				{
+					// exit from switch and show message
+					$msg .= "🚷 /:";
+					break;
+				}
+				$time_start = \lib\utility::date('H:i', $this->start, 'default');
+
+				$msg        .= $time_start. ' '. T_('to'). ' '. $time_now;
+
+				// add minus and plus if exist
+				if($this->plus > 0 )
+				{
+					$msg .= "\n➕ ". $this->plus;
+				}
+				if($this->minus > 0)
+				{
+					$msg .= "\n➖ ". $this->minus;
+				}
+				$total = floor($total / 60). ':'. floor($total % 60);
+				$msg .= "\n🕗 ". $total;
+				break;
+
+
+			default:
+				break;
+		}
+
+		// send telegram message
+		$tg = self::send_telegram($msg);
+	}
+
+
+	private static function list_online()
+	{
 		$list = \lib\db\users::get_all();
-		$msg = "🔬 \n";
+		$msg  = "";
 		foreach ($list as $key => $value) {
 		 	if($value['hour_start'] == null) {
-		 		$msg .= "😴 " . T_($value['displayname']) . "\n";
+		 		$msg .= "💤 " . T_($value['displayname']) . "\n";
 		 	}else{
 		 		$msg .= "❤ " . T_($value['displayname']) . "\n";
 		 	}
 		}
+		$msg .= "‌";
 		self::send_telegram($msg);
 	}
 }
