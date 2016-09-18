e41621e9ad716ba3577b083a2e5d3ad5158f9970
Javad <J.Evazzadeh@live.com>
2016-09-18 20:30:36 +0430

    fix view problem to show

--- content_ganje/home/view.php
+++ content_ganje/home/view.php
@@ -8,14 +8,13 @@ class view extends \mvc\view
 		if($this->module() === 'home')
 		{
 			$this->data->bodyclass  = 'unselectable register';
+			//----- list of users
+			$this->data->datatable      = $this->model()->get_list_of_users();
 		}
 		else
 		{
 			$this->data->bodyclass  = 'unselectable';
 		}
-		//----- list of users
-		$this->data->datatable      = $this->model()->get_list_of_users();
-
 
 		$this->data->site['title']  = T_("Ganje");
 		$this->data->site['desc']   = T_("Free & open source attendance service!");
@@ -26,7 +25,35 @@ class view extends \mvc\view
 		$deadline                      = strtotime("2016/10/22");
 		$this->data->deadline          = ['title' => T_('Demo'), 'value' => '', 'date' => $deadline];
 		$this->data->deadline['value'] = floor(abs(time() - $deadline) / (60 * 60 * 24));
+	}
 
+
+	/**
+	 * [get_default_values description]
+	 * @param  [type] $_args [description]
+	 * @return [type]        [description]
+	 */
+	public function get_default_values($_args)
+	{
+		$year_min     = '2016';
+		$year_current = \lib\utility::date('Y', false, 'current', false);
+		if(substr(\lib\router::get_storage('language'), 0, 2) == 'fa')
+		{
+			$year_min = '1395';
+		}
+		$year_default = $_args->get_date(1);
+
+		if($year_min == $year_current && $year_default != $year_current)
+		{
+			$year_default = '0000';
+		}
+
+		$month_default = $_args->get_date(2)? $_args->get_date(2): '00';
+		$day_default   = $_args->get_date(3)? $_args->get_date(3): '00';
+
+		$this->data->default_day   = ['val' => $day_default];
+		$this->data->default_month = ['val' => $month_default];
+		$this->data->default_year  = ['min' => $year_min, 'max' => $year_current, 'val' => $year_default];
 	}
 }
 ?>
--- content_ganje/status/view.php
+++ content_ganje/status/view.php
@@ -1,32 +1,14 @@
 <?php
 namespace content_ganje\status;
 
-class view extends \mvc\view
+class view extends \content_ganje\home\view
 {
 	function view_status($_args)
 	{
 		$this->data->et     = $_args->api_callback;
 
-		$year_min     = '2016';
-		$year_current = \lib\utility::date('Y', false, 'current', false);
-		if(substr(\lib\router::get_storage('language'), 0, 2) == 'fa')
-		{
-			$year_min = '1395';
-		}
-		$year_default = $_args->get_date(1);
-
-		if($year_min == $year_current && $year_default != '0000')
-		{
-			$year_default = '0000';
-		}
-
-		$month_default = $_args->get_date(2)? $_args->get_date(2): '00';
-		$day_default = $_args->get_date(3)? $_args->get_date(3): '00';
-
-
-		$this->data->default_day   = ['val' => $month_default];
-		$this->data->default_month = ['val' => $day_default];
-		$this->data->default_year  = ['min' => $year_min, 'max' => $year_current, 'val' => $year_default];
+		// set default values for dates
+		$this->get_default_values($_args);
 	}
 }
 ?>
