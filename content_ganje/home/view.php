031d66a160f930d24c2cd4dc0c7871dffa9abaa7
Javad <J.Evazzadeh@live.com>
2016-10-23 19:49:57 +0330

    update trans

--- content_ganje/home/view.php
+++ content_ganje/home/view.php
@@ -22,8 +22,8 @@ class view extends \mvc\view
 		$this->data->module         = $this->module();
 
 		// add deadline of projects
-		$deadline                        = strtotime("2016/10/23");
-		$this->data->deadline            = ['title' => T_('Sarshomar Demo'), 'value' => '', 'date' => $deadline, 'class' => '', 'start' => 45];
+		$deadline                        = strtotime("2016/11/09");
+		$this->data->deadline            = ['title' => T_('Sarshomar Alfa'), 'value' => '', 'date' => $deadline, 'class' => '', 'start' => 15];
 		$this->data->deadline['value']   = floor(($deadline - time()) / (60 * 60 * 24));
 		// calc percent of time form start of this perion until end of it
 		if($this->data->deadline['value'] <= 0)
