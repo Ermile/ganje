b14841bb34dbd61c47a204717e602a841f64f663
Javad <J.Evazzadeh@live.com>
2016-12-27 18:53:02 +0330

    update trans

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -27,9 +27,9 @@ private function transtext()
 	echo T_("Date");                                                                  // Line 57
 	echo T_("Start time");                                                            // Line 58
 	echo T_("End time");                                                              // Line 74
-	echo T_("Minus");                                                                 // Line 82
+	echo T_("Minus");                                                                 // Line 86
 	echo T_("(min)");                                                                 // Line 61
-	echo T_("Plus");                                                                  // Line 83
+	echo T_("Plus");                                                                  // Line 87
 	echo T_("Cancel");                                                                // Line 76
 	echo T_("or");                                                                    // Line 77
 	echo T_("Insert");                                                                // Line 78
@@ -40,13 +40,13 @@ private function transtext()
 	echo T_("days left");                                                             // Line 20
 	echo T_("Enter");                                                                 // Line 42
 	echo T_("Exit");                                                                  // Line 42
-	echo T_("Enter Time");                                                            // Line 78
-	echo T_("Difference between enter and exit");                                     // Line 79
-	echo T_("Operation Time");                                                        // Line 79
-	echo T_("Exit Time");                                                             // Line 80
-	echo T_("Total");                                                                 // Line 84
-	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set exit time.");         // Line 85
-	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set enter time.");        // Line 90
+	echo T_("Enter Time");                                                            // Line 82
+	echo T_("Difference between enter and exit");                                     // Line 83
+	echo T_("Operation Time");                                                        // Line 83
+	echo T_("Exit Time");                                                             // Line 84
+	echo T_("Total");                                                                 // Line 88
+	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set exit time.");         // Line 89
+	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set enter time.");        // Line 94
 
 	//------------------------------------------------------content_ganje\home\et.html
 	echo T_("Data not exist!");                                                       // Line 69
@@ -57,9 +57,6 @@ private function transtext()
 	echo T_("all");                                                                   // Line 98
 	echo T_("Work with Ermile in your own language");                                 // Line 117
 	echo T_("Export to CSV");                                                         // Line 126
-
-	//----------------------------------------------content_ganje\summary\display.html
-	echo T_("Export");                                                                // Line 22
 
 	//--------------------------------------------includes\languages\trans_static.html
 	echo T_("Saloos");                                                                // Line 7 Seperate
