e9e1fcd4d9d6e8a198f0412bdf419cec3aa2d6af
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-02-04 15:25:40 +0330

    update trans for saman

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -4,7 +4,7 @@ private function transtext()
 
 	//-------------------------------------------------------content\home\display.html
 	echo T_("Ganje, first one");                                                      // Line 8
-	echo T_("Ganje");                                                                 // Line 63 Seperate
+	echo T_("Ganje");                                                                 // Line 67 Seperate
 	echo T_("Ganje: this is a tejarak online attendance beginning Web-based which work without any complexities of existing ones.");// Line 47
 	echo T_("Integration with services which will soon be public lead no need for small businesses, costly and inconvenience.");// Line 47
 	echo T_("About");                                                                 // Line 56
@@ -23,42 +23,42 @@ private function transtext()
 	echo T_("Some right reserved!");                                                  // Line 28
 
 	//------------------------------------------------content_ganje\admin\display.html
-	echo T_("Add new record");                                                        // Line 46 Seperate
-	echo T_("Date");                                                                  // Line 57
-	echo T_("Start time");                                                            // Line 58
-	echo T_("End time");                                                              // Line 74
-	echo T_("Minus");                                                                 // Line 87
-	echo T_("(min)");                                                                 // Line 61
-	echo T_("Plus");                                                                  // Line 94
-	echo T_("Cancel");                                                                // Line 76
-	echo T_("or");                                                                    // Line 77
-	echo T_("Insert");                                                                // Line 78
-	echo T_("Edit record");                                                           // Line 73 Seperate
+	echo T_("Add new record");                                                        // Line 47 Seperate
+	echo T_("Date");                                                                  // Line 58
+	echo T_("Start time");                                                            // Line 59
+	echo T_("End time");                                                              // Line 75
+	echo T_("Minus");                                                                 // Line 89
+	echo T_("(min)");                                                                 // Line 62
+	echo T_("Plus");                                                                  // Line 96
+	echo T_("Cancel");                                                                // Line 77
+	echo T_("or");                                                                    // Line 78
+	echo T_("Insert");                                                                // Line 79
+	echo T_("Edit record");                                                           // Line 74 Seperate
 
 	//-------------------------------------------------content_ganje\home\display.html
 	echo T_("In the Name of God");                                                    // Line 8
 	echo T_("days left");                                                             // Line 20
-	echo T_("Enter");                                                                 // Line 43
-	echo T_("Exit");                                                                  // Line 43
-	echo T_("Dashboard");                                                             // Line 47
-	echo T_("Enter Time");                                                            // Line 83
-	echo T_("Difference between enter and exit");                                     // Line 84
-	echo T_("Operation Time");                                                        // Line 84
-	echo T_("Exit Time");                                                             // Line 85
-	echo T_("Total");                                                                 // Line 88
-	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set exit time.");         // Line 89
-	echo T_("I had gone out for Ermile!");                                            // Line 96
-	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set enter time.");        // Line 102
+	echo T_("Enter");                                                                 // Line 44
+	echo T_("Exit");                                                                  // Line 44
+	echo T_("Dashboard");                                                             // Line 48
+	echo T_("Enter Time");                                                            // Line 85
+	echo T_("Difference between enter and exit");                                     // Line 86
+	echo T_("Operation Time");                                                        // Line 86
+	echo T_("Exit Time");                                                             // Line 87
+	echo T_("Total");                                                                 // Line 90
+	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set exit time.");         // Line 91
+	echo T_("I had gone out for Ermile!");                                            // Line 98
+	echo T_("Press <kbd>Ctrl+Enter</kbd> or <kbd>*</kbd> to set enter time.");        // Line 100
 
 	//------------------------------------------------------content_ganje\home\et.html
 	echo T_("Data not exist!");                                                       // Line 69
 
 	//--------------------------------------------------content_ganje\home\layout.html
-	echo T_("Select all months");                                                     // Line 53
-	echo T_("Enter year");                                                            // Line 90
-	echo T_("all");                                                                   // Line 98
-	echo T_("Work with Ermile in your own language");                                 // Line 117
-	echo T_("Export to CSV");                                                         // Line 126
+	echo T_("Select all months");                                                     // Line 54
+	echo T_("Enter year");                                                            // Line 91
+	echo T_("all");                                                                   // Line 99
+	echo T_("Work with Ermile in your own language");                                 // Line 118
+	echo T_("Export to CSV");                                                         // Line 127
 
 	//--------------------------------------------includes\languages\trans_static.html
 	echo T_("Saloos");                                                                // Line 7 Seperate
@@ -81,37 +81,40 @@ private function transtext()
 	echo T_("Majid Hamidian");                                                        // Line 26 Seperate
 	echo T_("Moslem Hamzehnejadi");                                                   // Line 27 Seperate
 	echo T_("MohamadHosein Hamzehnejadi");                                            // Line 28 Seperate
-	echo T_("hours in this week");                                                    // Line 30 Seperate
-	echo T_("hours in this month");                                                   // Line 31 Seperate
-	echo T_("hours remote in this month");                                            // Line 32 Seperate
-	echo T_("hours in idle this month");                                              // Line 33 Seperate
-	echo T_("Founder & CEO");                                                         // Line 35 Seperate
-	echo T_("Founder and CEO");                                                       // Line 36 Seperate
-	echo T_("CEO & Solution Designer");                                               // Line 37 Seperate
-	echo T_("CEO and Solution Designer");                                             // Line 38 Seperate
-	echo T_("CEO");                                                                   // Line 39 Seperate
-	echo T_("Solution Designer");                                                     // Line 40 Seperate
-	echo T_("Full Stack Developer");                                                  // Line 41 Seperate
-	echo T_("Backend Developer");                                                     // Line 42 Seperate
-	echo T_("Frontend Designer");                                                     // Line 43 Seperate
-	echo T_("UI Designer");                                                           // Line 44 Seperate
-	echo T_("Graphic Designer");                                                      // Line 45 Seperate
-	echo T_("Shareholder");                                                           // Line 46 Seperate
-	echo T_("Chrikt");                                                                // Line 47 Seperate
-	echo T_("Content Expert");                                                        // Line 49 Seperate
-	echo T_("Service");                                                               // Line 50 Seperate
-	echo T_("UI Developer");                                                          // Line 51 Seperate
-	echo T_("Frontend Developer");                                                    // Line 52 Seperate
-	echo T_("Research & Develepment");                                                // Line 53 Seperate
-	echo T_("Research &amp; Develepment");                                            // Line 54 Seperate
-	echo T_("UI Expert");                                                             // Line 55 Seperate
-	echo T_("Android Developer");                                                     // Line 56 Seperate
-	echo T_("IOS Developer");                                                         // Line 57 Seperate
-	echo T_("WP Developer");                                                          // Line 58 Seperate
-	echo T_("name");                                                                  // Line 60 Seperate
-	echo T_("summary");                                                               // Line 61 Seperate
-	echo T_("ganje");                                                                 // Line 62 Seperate
-	echo T_(" ");                                                                     // Line 67 Seperate
+	echo T_("Saman Soltani");                                                         // Line 29 Seperate
+	echo T_("hours in this week");                                                    // Line 31 Seperate
+	echo T_("hours in this month");                                                   // Line 32 Seperate
+	echo T_("hours remote in this month");                                            // Line 33 Seperate
+	echo T_("hours in idle this month");                                              // Line 34 Seperate
+	echo T_("Founder & CEO");                                                         // Line 36 Seperate
+	echo T_("Founder and CEO");                                                       // Line 37 Seperate
+	echo T_("CEO & Solution Designer");                                               // Line 38 Seperate
+	echo T_("CEO and Solution Designer");                                             // Line 39 Seperate
+	echo T_("CEO");                                                                   // Line 40 Seperate
+	echo T_("Solution Designer");                                                     // Line 41 Seperate
+	echo T_("Full Stack Developer");                                                  // Line 42 Seperate
+	echo T_("Backend Developer");                                                     // Line 43 Seperate
+	echo T_("Frontend Designer");                                                     // Line 44 Seperate
+	echo T_("UI Designer");                                                           // Line 45 Seperate
+	echo T_("Graphic Designer");                                                      // Line 46 Seperate
+	echo T_("Shareholder");                                                           // Line 47 Seperate
+	echo T_("Chrikt");                                                                // Line 48 Seperate
+	echo T_("Content Expert");                                                        // Line 50 Seperate
+	echo T_("Service");                                                               // Line 51 Seperate
+	echo T_("UI Developer");                                                          // Line 52 Seperate
+	echo T_("Frontend Developer");                                                    // Line 53 Seperate
+	echo T_("Research & Develepment");                                                // Line 54 Seperate
+	echo T_("Research &amp; Develepment");                                            // Line 55 Seperate
+	echo T_("UI Expert");                                                             // Line 56 Seperate
+	echo T_("Android Developer");                                                     // Line 57 Seperate
+	echo T_("IOS Developer");                                                         // Line 58 Seperate
+	echo T_("WP Developer");                                                          // Line 59 Seperate
+	echo T_("Nothing!");                                                              // Line 60 Seperate
+	echo T_("Traveler");                                                              // Line 61 Seperate
+	echo T_("name");                                                                  // Line 64 Seperate
+	echo T_("summary");                                                               // Line 65 Seperate
+	echo T_("ganje");                                                                 // Line 66 Seperate
+	echo T_(" ");                                                                     // Line 71 Seperate
 
 }
 ?>
